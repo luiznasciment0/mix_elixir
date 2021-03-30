365a276fb1301a5cc0f994014fb27b7683b7b173
Luiz Nascimento <luizp.nascimento@outlook.com>
2021-03-30 02:26:52 -0300

    foto

--- foto/lib/foto.ex
+++ foto/lib/foto.ex
@@ -1,18 +1,22 @@
 defmodule Foto do
-  @moduledoc """
-  Documentation for `Foto`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Foto.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  alias Foto.Imagem
+  defmodule Imagem do
+    defstruct hex: nil, cor: nil
   end
+
+  def main(input) do
+    input
+    |> hash_input
+    |> definir_cor
+  end
+
+  defp hash_input(input) do
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list
+
+    %Foto.Imagem{hex: hex}
+  end
+
+  defp definir_cor(%Imagem{hex: [r, g, b | _resto]} = imagem), do: %Imagem{imagem | cor: [r, g, b]}
 end
